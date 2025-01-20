2d7271778ac783244c12c5905cd52b3b1f3cb7e3
Joostlek <joostlek@outlook.com>
2025-01-20 15:25:48 +0100

    Add test

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,21 +2,43 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-from aioresponses import aioresponses
+import pytest
+from aiohttp import ClientError
+from aioresponses import aioresponses, CallbackResult
 
 from tests import load_fixture
 from tests.const import MOCK_URL
+
+from switchbot_api import CannotConnect
 
 if TYPE_CHECKING:
     from switchbot_api import SwitchBotAPI
     from syrupy import SnapshotAssertion
 
 
-async def test_device_list(
-    responses: aioresponses,
+async def test_client_error(
     client: SwitchBotAPI,
+    responses: aioresponses,
+) -> None:
+    """Test client error."""
+
+    async def response_handler(_: str, **_kwargs: Any) -> CallbackResult:
+        """Response handler for this test."""
+        raise ClientError
+
+    responses.get(
+        f"{MOCK_URL}/devices",
+        callback=response_handler,
+    )
+    with pytest.raises(CannotConnect):
+        await client.list_devices()
+
+
+async def test_device_list(
+    client: SwitchBotAPI,
+    responses: aioresponses,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test listing devices."""
