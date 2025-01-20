9fd00db44970f54b8c704f4700a4197b2423ac35
Joostlek <joostlek@outlook.com>
2025-01-20 17:23:42 +0100

    Add device fixtures

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from aioresponses import aioresponses
+import pytest
 
 from tests import load_fixture
 from tests.const import MOCK_URL
@@ -26,3 +27,26 @@ async def test_device_list(
         body=load_fixture("device_list.json"),
     )
     assert await client.list_devices() == snapshot
+
+
+@pytest.mark.parametrize(
+    "device_fixture",
+    [
+        "curtain",
+        "curtain3",
+        "hub_2",
+    ],
+)
+async def test_device_status(
+    client: SwitchBotAPI,
+    responses: aioresponses,
+    snapshot: SnapshotAssertion,
+    device_fixture: str,
+) -> None:
+    """Test fetching device status."""
+    responses.get(
+        f"{MOCK_URL}/devices/abc/status",
+        status=200,
+        body=load_fixture(f"{device_fixture}.json"),
+    )
+    assert await client.get_status("abc") == snapshot
