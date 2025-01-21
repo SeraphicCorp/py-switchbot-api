4251bbd89bce54477fee8b3a78e79a4daa2446cb
Joost Lekkerkerker <joostlek@outlook.com>
2025-01-21 10:19:50 +0100

    Remove double context (#21)
    * Remove double context

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,11 +2,13 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-from aioresponses import aioresponses
+from aiohttp import ClientError
+from aioresponses import CallbackResult, aioresponses
 import pytest
 
+from switchbot_api import SwitchBotConnectionError
 from tests import load_fixture
 from tests.const import MOCK_URL
 
@@ -15,9 +17,27 @@ if TYPE_CHECKING:
     from syrupy import SnapshotAssertion
 
 
-async def test_device_list(
-    responses: aioresponses,
+async def test_client_error(
     client: SwitchBotAPI,
+    responses: aioresponses,
+) -> None:
+    """Test client error."""
+
+    async def response_handler(_: str, **_kwargs: Any) -> CallbackResult:
+        """Response handler for this test."""
+        raise ClientError
+
+    responses.get(
+        f"{MOCK_URL}/devices",
+        callback=response_handler,
+    )
+    with pytest.raises(SwitchBotConnectionError):
+        await client.list_devices()
+
+
+async def test_device_list(
+    client: SwitchBotAPI,
+    responses: aioresponses,
     snapshot: SnapshotAssertion,
 ) -> None:
     """Test listing devices."""
