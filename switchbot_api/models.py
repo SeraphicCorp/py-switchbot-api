cbb08bd3bf4b06c4970d948893c9314533e89698
XiaoLing-git <samuel.ling.xiao@qq.com>
2026-04-10 15:25:01 +0800

    added DeviceSupportMap

--- switchbot_api/models.py
+++ switchbot_api/models.py
@@ -15,19 +15,179 @@ class PowerState(Enum):
 class EntityType(Enum):
     """Entity Type."""
 
-    BINARY_SENSOR = "binary_sensor"
-    BUTTON = "button"
-    CLIMATE = "climate"
-    COVER = "cover"
-    FAN = "fan"
-    HUMIDIFIER="humidifier"
-    IMAGE = "image"
-    LIGHT = "light"
-    LOCK ="lock"
-    SENSOR = "sensor"
-    SWITCH= "switch"
-    VACUUM = "vacuum"
+    BINARY_SENSOR = "binary_sensors"
+    BUTTON = "buttons"
+    CLIMATE = "climates"
+    COVER = "covers"
+    FAN = "fans"
+    HUMIDIFIER = "humidifiers"
+    IMAGE = "images"
+    LIGHT = "lights"
+    LOCK = "locks"
+    SENSOR = "sensors"
+    SWITCH = "switches"
+    VACUUM = "vacuums"
 
+
+DeviceSupportMap: dict[str, dict[str, bool | list[EntityType]]] = {
+    "Smart Radiator Thermostat": {
+        "webhook": False,
+        "entity_list": [EntityType.CLIMATE, EntityType.SENSOR],
+    },
+    "Relay Switch 1PM": {
+        "webhook": False,
+        "entity_list": [EntityType.SWITCH, EntityType.SENSOR],
+    },
+    "Relay Switch 1": {"webhook": False, "entity_list": [EntityType.SWITCH]},
+    "Relay Switch 2PM": {
+        "webhook": False,
+        "entity_list": [EntityType.SWITCH, EntityType.SENSOR],
+    },
+    "K10+": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "K10+ Pro": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "Robot Vacuum Cleaner S1": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "Robot Vacuum Cleaner S1 Plus": {
+        "webhook": True,
+        "entity_list": [EntityType.VACUUM],
+    },
+    "K20+ Pro": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "Robot Vacuum Cleaner K10+ Pro Combo": {
+        "webhook": True,
+        "entity_list": [EntityType.VACUUM],
+    },
+    "Robot Vacuum Cleaner S10": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "Robot Vacuum Cleaner S20": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "S20": {"webhook": True, "entity_list": [EntityType.VACUUM]},
+    "Robot Vacuum Cleaner K11 Plus": {
+        "webhook": True,
+        "entity_list": [EntityType.VACUUM],
+    },
+    "Smart Lock": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Smart Lock Lite": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Smart Lock Pro": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Smart Lock Ultra": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Smart Lock Vision": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Smart Lock Vision Pro": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Smart Lock Pro Wifi": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Lock Vision": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Lock Vision Pro": {
+        "webhook": False,
+        "entity_list": [EntityType.LOCK, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Motion Sensor": {
+        "webhook": True,
+        "entity_list": [EntityType.BINARY_SENSOR, EntityType.SENSOR],
+    },
+    "Contact Sensor": {
+        "webhook": True,
+        "entity_list": [EntityType.BINARY_SENSOR, EntityType.SENSOR],
+    },
+    "Presence Sensor": {
+        "webhook": True,
+        "entity_list": [EntityType.BINARY_SENSOR, EntityType.SENSOR],
+    },
+    "Hub 3": {
+        "webhook": True,
+        "entity_list": [EntityType.BINARY_SENSOR, EntityType.SENSOR],
+    },
+    "Water Detector": {
+        "webhook": True,
+        "entity_list": [EntityType.BINARY_SENSOR, EntityType.SENSOR],
+    },
+    "Battery Circulator Fan": {
+        "webhook": False,
+        "entity_list": [EntityType.FAN, EntityType.SENSOR],
+    },
+    "Standing Fan": {
+        "webhook": False,
+        "entity_list": [EntityType.FAN, EntityType.SENSOR],
+    },
+    "Circulator Fan": {"webhook": False, "entity_list": [EntityType.FAN]},
+    "Curtain": {
+        "webhook": False,
+        "entity_list": [EntityType.COVER, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Curtain3": {
+        "webhook": False,
+        "entity_list": [EntityType.COVER, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Roller Shade": {
+        "webhook": False,
+        "entity_list": [EntityType.COVER, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Blind Tilt": {
+        "webhook": False,
+        "entity_list": [EntityType.COVER, EntityType.SENSOR, EntityType.BINARY_SENSOR],
+    },
+    "Strip Light": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Strip Light 3": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Floor Lamp": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Color Bulb": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "RGBICWW Floor Lamp": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "RGBICWW Strip Light": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Ceiling Light": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Ceiling Light Pro": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "RGBIC Neon Rope Light": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "RGBIC Neon Wire Rope Light": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Candle Warmer Lamp": {"webhook": False, "entity_list": [EntityType.LIGHT]},
+    "Humidifier2": {"webhook": False, "entity_list": [EntityType.HUMIDIFIER]},
+    "Humidifier": {
+        "webhook": False,
+        "entity_list": [EntityType.HUMIDIFIER, EntityType.SENSOR],
+    },
+    "Home Climate Panel": {
+        "webhook": False,
+        "entity_list": [EntityType.BINARY_SENSOR, EntityType.SENSOR],
+    },
+    "AI Art Frame": {
+        "webhook": False,
+        "entity_list": [EntityType.BUTTON, EntityType.SENSOR, EntityType.IMAGE],
+    },
+    "WeatherStation": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "Meter": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "MeterPlus": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "WoIOSensor": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "Hub 2": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "MeterPro": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "MeterPro(CO2)": {"webhook": False, "entity_list": [EntityType.SENSOR]},
+    "Plug": {"webhook": False, "entity_list": [EntityType.SWITCH]},
+    "Plug Mini (US)": {
+        "webhook": False,
+        "entity_list": [EntityType.SENSOR, EntityType.SWITCH],
+    },
+    "Plug Mini (JP)": {
+        "webhook": False,
+        "entity_list": [EntityType.SENSOR, EntityType.SWITCH],
+    },
+    "Plug Mini (EU)": {
+        "webhook": False,
+        "entity_list": [EntityType.SENSOR, EntityType.SWITCH],
+    },
+}
 
 
 class BatteryCirculatorFanMode(StrEnum):
